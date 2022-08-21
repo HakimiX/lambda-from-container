9213758c7d4a92650385cf509f7c2e6651728bbf
HakimiX <mustafa.hakimi@outlook.dk>
2022-08-21 12:15:47 +0200

    add lambda from container stack

--- lambda_from_container/lambda_from_container_stack.py
+++ lambda_from_container/lambda_from_container_stack.py
@@ -2,8 +2,12 @@ from aws_cdk import (
     aws_lambda as _lambda,
     aws_ecr as ecr,
     Stack,
-    Aws
+    Aws,
+    App,
+    Duration
 )
+import os
+import typing
 from constructs import Construct
 
 class LambdaFromContainerStack(Stack):
@@ -21,9 +25,39 @@ class LambdaFromContainerStack(Stack):
         use_pre_existing_iamge = False
 
         if (use_pre_existing_iamge):
+            # If the image exists, use the the pre-existing image. 
             ecr_repository = ecr.Repository.from_repository_attributes(
                 self, 
                 id="ECR",
                 repository_arn="arn:aws:ecr:{0}:{1}".format(Aws.REGION, Aws.ACCOUNT_ID),
                 repository_name=image_name
             )             
+
+            # pull the existing image 
+            ecr_image = typing.cast("aws_lambda.Code", _lambda.EcrImageCode(
+                repository=ecr_repository
+            ))
+        else:
+            # if the image does not exist, create the image 
+            ecr_image = _lambda.EcrImageCode.from_asset_image(
+                directory=os.path.join(os.getcwd(), "lambda-image")
+            )
+
+
+        # Lambda function 
+        _lambda.Function(
+            self, 
+            id="lambdaContainerFunction",
+            description="sample lambda container function",
+            code = ecr_image,
+            # Handler must be "FROM_IMAGE" when provisioning lambda from image
+            handler = _lambda.Handler.FROM_IMAGE,
+            runtime = _lambda.Runtime.FROM_IMAGE,
+            environment={
+                "HELLO": "WORLD"
+            },
+            function_name="sampleContainerFuntion",
+            memory_size=128,
+            reserved_concurrent_executions=10,
+            timeout=Duration.seconds(10)
+        )
