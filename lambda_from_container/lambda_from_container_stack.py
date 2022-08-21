3e77443a0f4fce35c8c27d6d8d6f0e27e9b421a5
HakimiX <x@OLDKHQ189425.corp.novocorp.net>
2022-08-21 11:32:07 +0200

    ignore ide settings, and implement ecr

--- lambda_from_container/lambda_from_container_stack.py
+++ lambda_from_container/lambda_from_container_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
-    # Duration,
+    aws_lambda as _lambda,
+    aws_ecr as ecr,
     Stack,
-    # aws_sqs as sqs,
+    Aws
 )
 from constructs import Construct
 
@@ -10,10 +11,19 @@ class LambdaFromContainerStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        image_name = "lambdaContainerFunction"
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "LambdaFromContainerQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        """
+        if use_pre_existing_image is True
+        then use an image that already exists in ECR.
+        Otherwise, build a new image
+        """
+        use_pre_existing_iamge = False
+
+        if (use_pre_existing_iamge):
+            ecr_repository = ecr.Repository.from_repository_attributes(
+                self, 
+                id="ECR",
+                repository_arn="arn:aws:ecr:{0}:{1}".format(Aws.REGION, Aws.ACCOUNT_ID),
+                repository_name=image_name
+            )             
